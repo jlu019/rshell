6e4e84d9070c12c2ca70fab33d57ebc752673239
Jamie Lu <jlu019@hammer.cs.ucr.edu>
2015-01-23 21:07:46 -0800

    added case of comments

--- rshell.cpp
+++ rshell.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
 
 using namespace std;
 
@@ -15,18 +16,25 @@ void connect()
 }
 
 int main(
-	char inp[];
+	string inp;
+	string inp2;
+	char* user = getlogin();
+	char* host = gethostname();
 	char *tok;
-	unsigned i = 0;
 	
-	cout << "$ ";
-	while()
+	cout << user << "@" << host << "$ ";
+	getline(inp);
+
+	for(unsigned i = 0; i < inp.size(); ++i)
 	{
-		cin << inp[i];
-		if(inp[i] == endl)
-			break;	
-		++i;
+		if(inp[i] == '#')
+			break;
+		inp2.push_back(inp[i]);
+		
 	}
 	
-	tok = strtok(inp," ");
+	tok = strtok(inp, " ");
+	
+	//perror("
+	tok = strtok(NULL, " ");
 }
