98e83deb5fdc09bc0481eb99fe9ecf2be21563b5
Jamie Lu <jlu019@hammer.cs.ucr.edu>
2015-01-23 23:15:42 -0800

    fixed bugs

--- rshell.cpp
+++ rshell.cpp
@@ -1,40 +1,57 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
 
 using namespace std;
 
-void command()
+int main()
 {
-
-
-}
-
-void connect()
-{
-
-}
-
-int main(
 	string inp;
 	string inp2;
+	
+	size_t len = 200;
 	char* user = getlogin();
-	char* host = gethostname();
+	char host[len];
+	int gt_host = gethostname(host, len);
+	if(gt_host == -1)
+		perror("host does not exist");
 	char *tok;
 	
-	cout << user << "@" << host << "$ ";
-	getline(inp);
-
-	for(unsigned i = 0; i < inp.size(); ++i)
+	while(1)
 	{
-		if(inp[i] == '#')
-			break;
-		inp2.push_back(inp[i]);
 		
+		cout << user << "@" << host << "$ ";
+		getline(cin, inp);
+
+		for(unsigned i = 0; i < inp.size(); ++i)
+		{
+			if(inp[i] == '#')
+				break;
+			inp2.push_back(inp[i]);
+			
+		}
+		for(unsigned i = 1; i < inp2.size(); ++i)
+		{
+			if(inp2[i] == '&' && inp2[i-1] != ' ')
+			{
+				inp2.insert(i-1, " ");
+				if(inp2[i+1] == '&' && inp2[i+2] != ' ')
+				{}
+			}
+			if(inp2[i] == '|' && inp2[i-1] != ' ')
+			{
+				inp2.insert(i-1, " ");
+				if(inp2[i+1] == '|' && inp2[i+2] != ' ')
+				{}
+			}
+
+		}	
+		//tok = strtok(inp2, " ");
+			
+		//perror("
+		tok = strtok(NULL, " ");
 	}
-	
-	tok = strtok(inp, " ");
-	
-	//perror("
-	tok = strtok(NULL, " ");
+	return 0;
 }
