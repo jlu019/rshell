993a161deb2d044e811e0c4c1d158ca906c6c70a
Jamie Lu <jlu019@hammer.cs.ucr.edu>
2015-01-23 16:08:11 -0800

    added the first code

---
+++ rshell.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void command()
+{
+
+
+}
+
+void connect()
+{
+
+}
+
+int main(
+	char inp[];
+	char *tok;
+	unsigned i = 0;
+	
+	cout << "$ ";
+	while()
+	{
+		cin << inp[i];
+		if(inp[i] == endl)
+			break;	
+		++i;
+	}
+	
+	tok = strtok(inp," ");
+}
